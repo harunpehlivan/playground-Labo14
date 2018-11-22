406f15c78cf3c4bdccc0a03a06a1a0d494e8a023
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-22 23:16:42 +0100

    Update strlength.h

--- contains/strlength.h
+++ contains/strlength.h
@@ -1,6 +1,5 @@
 size_t strlength(char *chaine) {
-	// Ecrire le code de la fonction contains
-	// la fonction renvoie l'indice de la première occurence de l'élément si il est présent dans le vecteur
-	// la fonction renvoie -1 si l'élément n'est pas trouvé
+	// Ecrire le code de la fonction strlength
+	// la fonction renvoie la taille de la chaine 'chaine'
 	return 0;
 }
